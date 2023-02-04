fed11f456909db6ffe5ac888b43aeb171887ec79
Cactri R.L <109222990+CactriRL@users.noreply.github.com>
2023-02-04 11:16:18 -0400

    #Document
    Signed-off-by: Cactri R.L. <109222990+CactriRL@users.noreply.github.com>

--- 0C93B212-VEEHpuBliC5C18-4996-8718-54967B.bat
+++ 0C93B212-VEEHpuBliC5C18-4996-8718-54967B.bat
@@ -1,10 +1,10 @@
 cls
 @ECHO OFF
-title Folder #VictorEmanuel
+title Folder #Document
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST #VictorEmanuel goto MDLOCKER
+if NOT EXIST #Document goto MDLOCKER
 :CONFIRM
-echo Quiere ocultar la #VictorEmanuel? (S/N)
+echo Quiere ocultar la Documentacion de Victor Emanuel? (S/N)
 set/p "cho="
 if %cho%==S goto LOCK
 if %cho%==s goto LOCK
@@ -13,23 +13,23 @@ if %cho%==N goto END
 echo Invalid choice.
 goto CONFIRM
 :LOCK
-ren #VictorEmanuel "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren #Document "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 echo Folder locked
 goto End
 :UNLOCK
-echo Introduzca la contrasena para mostrar #VictorEmanuel
+echo Introduzca la contrasena para mostrar la documentacion de Victor Emanuel
 set/p "pass="
 if NOT %pass%== thenights goto FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" #VictorEmanuel
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" #Document
 echo Folder Unlocked successfully
 goto End
 AIL
 echo Invalid password
 goto end
 :MDLOCKER
-md #VictorEmanuel
-echo #VictorEmanuel created successfully
+md #Document
+echo #Document created successfully
 goto End
 :End
