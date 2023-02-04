474a2b39d9f4aea26c06060e3393c71effbd0440
Cactri R.L <109222990+CactriRL@users.noreply.github.com>
2023-02-04 11:12:45 -0400

    Update 0C93B212-VEEHpuBliC5C18-4996-8718-54967B.bat
    Signed-off-by: Cactri R.L. <109222990+CactriRL@users.noreply.github.com>

--- 0C93B212-VEEHpuBliC5C18-4996-8718-54967B.bat
+++ 0C93B212-VEEHpuBliC5C18-4996-8718-54967B.bat
@@ -20,7 +20,7 @@ goto End
 :UNLOCK
 echo Introduzca la contrasena para mostrar #VictorEmanuel
 set/p "pass="
-if NOT %pass%== theningth goto FAIL
+if NOT %pass%== thenights goto FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" #VictorEmanuel
 echo Folder Unlocked successfully
